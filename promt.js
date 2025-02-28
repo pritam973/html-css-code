3fe9b4727dad3f9411268b2b5f33ce8e7c32ad70
pritam973 <sahamamoni973@gmail.com>
2025-02-28 20:38:39 +0530

    change nma

---
+++ promt.js
@@ -0,0 +1,12 @@
+//alert("hello worlds")
+ let x =  prompt("Enter your name:","pritam")
+ let y =  confirm("do want me to print these name on the document page?")
+
+ if(y)
+ {
+    document.write(x)
+ }
+ else{
+    document.write("Error")
+ }
+
